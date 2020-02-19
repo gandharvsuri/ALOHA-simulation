972ee4ed5a7c1fd246778246421c85040fd20faf
gandharv <gandharv.suri@iiitb.org>
2020-02-19 21:01:16 +0530

    updated code

--- Pure Aloha/simulation.py
+++ Pure Aloha/simulation.py
@@ -1,11 +1,45 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+import time
+import random
 
-def pure_aloha_throughput(G):
+
+Simulation_duration = 10000
+
+def next_pkt(load):
+    return (-1/load)*np.log(random.random())
+
+def pure_aloha_throughput_theoretical(G):
     return G*np.exp(-2*G)    
 
+def pure_aloha_throughput(G):
+    time = 0
+    pkt = []
+    for i in range(0,Simulation_duration):
+        time += next_pkt(G)
+        pkt.append(time)
+
+
+    
+    success = 0
+    for i in range(1,len(pkt)-1):
+        if((pkt[i] - pkt[i-1]) >1 and (pkt[i+1] - pkt[i])>1):
+            success += 1
+
+    
+
+    return float(success)/len(pkt) * G
+
 def pure_aloha_sim(data):
+    data['Throughput Theoretical'] = data['Avg Load'].apply(pure_aloha_throughput_theoretical)
+    plt.plot(data['Avg Load'],data['Throughput Theoretical'],marker ='',color = 'red',linewidth='2.5')
+    plt.xlabel("Load offered (G)")
+    plt.ylabel("Throughput (S)")
+    plt.grid(True)
+    plt.savefig('Pure_Aloha_Theoretical.png')
+    plt.show() 
+
     data['Throughput'] = data['Avg Load'].apply(pure_aloha_throughput)
     plt.plot(data['Avg Load'],data['Throughput'],marker ='',color = 'red',linewidth='2.5')
     plt.xlabel("Load offered (G)")
@@ -17,8 +51,9 @@ def pure_aloha_sim(data):
 
 
 
+
 if __name__ == "__main__":
-    load = np.arange(0.0,2.1,0.1)
+    load = np.arange(0.1,2.1,0.1)
 
     data = { "Avg Load" : load}
     df = pd.DataFrame(data)
--- Slotted Aloha/simulation.py
+++ Slotted Aloha/simulation.py
@@ -1,11 +1,45 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+import time
+import random
 
-def slotted_aloha_throughput(G):
+
+Simulation_duration = 10000
+
+def next_pkt(load):
+    return (-1/load)*np.log(random.random())
+
+def slotted_aloha_throughput_theoretical(G):
     return G*np.exp(-G)    
 
-def pure_aloha_sim(data):
+def slotted_aloha_throughput(G):
+    time = 0
+    pkt = []
+    for i in range(0,Simulation_duration):
+        time += next_pkt(G)
+        pkt.append(np.ceil(time))
+
+
+    
+    success = 0
+    for x in range(1,len(pkt)-1):
+        if (pkt[x] != pkt[x-1]  and pkt[x+1] != pkt[x]):
+            success += 1
+
+    
+
+    return float(success)/len(pkt) * G
+
+def slotted_aloha_sim(data):
+    data['Throughput Theoretical'] = data['Avg Load'].apply(slotted_aloha_throughput_theoretical)
+    plt.plot(data['Avg Load'],data['Throughput Theoretical'],marker ='',color = 'red',linewidth='2.5')
+    plt.xlabel("Load offered (G)")
+    plt.ylabel("Throughput (S)")
+    plt.grid(True)
+    plt.savefig('Slotted_Aloha_Theoretical.png')
+    plt.show() 
+
     data['Throughput'] = data['Avg Load'].apply(slotted_aloha_throughput)
     plt.plot(data['Avg Load'],data['Throughput'],marker ='',color = 'red',linewidth='2.5')
     plt.xlabel("Load offered (G)")
@@ -17,10 +51,11 @@ def pure_aloha_sim(data):
 
 
 
+
 if __name__ == "__main__":
-    load = np.arange(0.0,2.1,0.1)
+    load = np.arange(0.1,2.1,0.1)
 
     data = { "Avg Load" : load}
     df = pd.DataFrame(data)
 
-    pure_aloha_sim(df)
+    slotted_aloha_sim(df)
