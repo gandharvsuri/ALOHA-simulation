c19658fd13ceca0a3d21789f5db4796ecd470ffd
gandharv <gandharv.suri@iiitb.org>
2020-02-19 16:20:40 +0530

    slotted Aloha

--- Slotted Aloha/simulation.py
+++ Slotted Aloha/simulation.py
@@ -2,16 +2,16 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
-def pure_aloha_throughput(G):
-    return G*np.exp(-2*G)    
+def slotted_aloha_throughput(G):
+    return G*np.exp(-G)    
 
 def pure_aloha_sim(data):
-    data['Throughput'] = data['Avg Load'].apply(pure_aloha_throughput)
+    data['Throughput'] = data['Avg Load'].apply(slotted_aloha_throughput)
     plt.plot(data['Avg Load'],data['Throughput'],marker ='',color = 'red',linewidth='2.5')
     plt.xlabel("Load offered (G)")
     plt.ylabel("Throughput (S)")
     plt.grid(True)
-    plt.savefig('Pure_Aloha.png')
+    plt.savefig('Slotted_Aloha.png')
     plt.show() 
 
 
